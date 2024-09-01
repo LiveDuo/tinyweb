8123e5afcc8b7cfe9e25a11f5231ea023b5d7bc2
Andreas Tzionis <andy0n7@gmail.com>
2024-09-01 11:28:54 +0300

    cleanup js

--- src/rust/src/utils/js.rs
+++ src/rust/src/utils/js.rs
@@ -11,54 +11,20 @@ pub const DOM_WINDOW: ExternRef = ExternRef { value: 2 };
 pub const DOM_DOCUMENT: ExternRef = ExternRef { value: 3 };
 pub const DOM_BODY: ExternRef = ExternRef { value: 4 };
 
-extern "C" {
-    fn js_register_function(start: f64, len: f64) -> f64;
-    fn js_invoke_function(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> f64;
-    fn js_invoke_function_and_return_object(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> i64;
-    fn js_invoke_function_and_return_bigint(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> i64;
-    fn js_invoke_function_and_return_string(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> usize;
-    fn js_invoke_function_and_return_array_buffer(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> usize;
-    fn js_invoke_function_and_return_bool(
-        fn_handle: f64,
-        parameters_start: *const u8,
-        parameters_length: usize,
-    ) -> f64;
-}
-
 #[derive(Copy, Clone)]
 pub struct JSFunction {
     pub fn_handle: f64,
 }
 
-//convert invoke parameters into bytes
-//assuming each parameter is preceded by a 32 bit integer indicating its type
-//0 = undefined
-//1 = null
-//2 = float-64
-//3 = bigint
-//4 = string (followed by 32-bit start and size of string in memory)
-//5 = extern ref
-//6 = array of float-64 (followed by 32-bit start and size of string in memory)
+// convert invoke parameters into bytes
+// assuming each parameter is preceded by a 32 bit integer indicating its type
+// 0 = undefined
+// 1 = null
+// 2 = float-64
+// 3 = bigint
+// 4 = string (followed by 32-bit start and size of string in memory)
+// 5 = extern ref
+// 6 = array of float-64 (followed by 32-bit start and size of string in memory)
 pub enum InvokeParam<'a> {
     Undefined,
     Null,
@@ -73,63 +39,43 @@ pub enum InvokeParam<'a> {
 }
 
 impl From<f64> for InvokeParam<'_> {
-    fn from(f: f64) -> Self {
-        InvokeParam::Float64(f)
-    }
+    fn from(f: f64) -> Self { InvokeParam::Float64(f) }
 }
 
 impl From<i32> for InvokeParam<'_> {
-    fn from(i: i32) -> Self {
-        InvokeParam::Float64(i as f64)
-    }
+    fn from(i: i32) -> Self { InvokeParam::Float64(i as f64) }
 }
 
 impl From<usize> for InvokeParam<'_> {
-    fn from(i: usize) -> Self {
-        InvokeParam::Float64(i as f64)
-    }
+    fn from(i: usize) -> Self { InvokeParam::Float64(i as f64) }
 }
 
 impl From<i64> for InvokeParam<'_> {
-    fn from(i: i64) -> Self {
-        InvokeParam::BigInt(i)
-    }
+    fn from(i: i64) -> Self { InvokeParam::BigInt(i) }
 }
 
 impl<'a> From<&'a str> for InvokeParam<'a> {
-    fn from(s: &'a str) -> Self {
-        InvokeParam::String(s)
-    }
+    fn from(s: &'a str) -> Self { InvokeParam::String(s) }
 }
 
 impl<'a> From<&'a ExternRef> for InvokeParam<'a> {
-    fn from(i: &'a ExternRef) -> Self {
-        InvokeParam::ExternRef(i)
-    }
+    fn from(i: &'a ExternRef) -> Self { InvokeParam::ExternRef(i) }
 }
 
 impl<'a> From<&'a [f32]> for InvokeParam<'a> {
-    fn from(a: &'a [f32]) -> Self {
-        InvokeParam::Float32Array(a)
-    }
+    fn from(a: &'a [f32]) -> Self { InvokeParam::Float32Array(a) }
 }
 
 impl<'a> From<&'a [f64]> for InvokeParam<'a> {
-    fn from(a: &'a [f64]) -> Self {
-        InvokeParam::Float64Array(a)
-    }
+    fn from(a: &'a [f64]) -> Self { InvokeParam::Float64Array(a) }
 }
 
 impl From<bool> for InvokeParam<'_> {
-    fn from(b: bool) -> Self {
-        InvokeParam::Bool(b)
-    }
+    fn from(b: bool) -> Self { InvokeParam::Bool(b) }
 }
 
 impl<'a> From<&'a [u32]> for InvokeParam<'a> {
-    fn from(a: &'a [u32]) -> Self {
-        InvokeParam::Uint32Array(a)
-    }
+    fn from(a: &'a [u32]) -> Self { InvokeParam::Uint32Array(a) }
 }
 
 fn param_to_bytes(params: &[InvokeParam]) -> Vec<u8> {
@@ -194,17 +140,31 @@ fn param_to_bytes(params: &[InvokeParam]) -> Vec<u8> {
     param_bytes
 }
 
+extern "C" {
+    fn js_register_function(ptr: f64, len: f64) -> f64;
+    fn js_invoke_function(fn_handle: f64, ptr: *const u8, len: usize) -> f64;
+    fn js_invoke_function_and_return_object(fn_handle: f64, ptr: *const u8, len: usize) -> i64;
+    fn js_invoke_function_and_return_bigint(fn_handle: f64, ptr: *const u8, len: usize) -> i64;
+    fn js_invoke_function_and_return_string(fn_handle: f64, ptr: *const u8, len: usize) -> usize;
+    fn js_invoke_function_and_return_array_buffer(fn_handle: f64, ptr: *const u8, len: usize) -> usize;
+    fn js_invoke_function_and_return_bool(fn_handle: f64, ptr: *const u8, len: usize) -> f64;
+}
+
+pub fn register_function(code: &str) -> JSFunction {
+    let start = code.as_ptr();
+    let len = code.len();
+    unsafe { JSFunction { fn_handle: js_register_function(start as usize as f64, len as f64), } }
+}
+
 impl JSFunction {
-    pub fn invoke(&self, params: &[InvokeParam]) -> f64
-where {
+    pub fn invoke(&self, params: &[InvokeParam]) -> f64 {
         let param_bytes = param_to_bytes(params);
         let mut me = ManuallyDrop::new(param_bytes);
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
         unsafe { js_invoke_function(self.fn_handle, ptr, length) }
     }
 
-    pub fn invoke_and_return_object(&self, params: &[InvokeParam]) -> ExternRef
-where {
+    pub fn invoke_and_return_object(&self, params: &[InvokeParam]) -> ExternRef {
         let param_bytes = param_to_bytes(params);
         let mut me = ManuallyDrop::new(param_bytes);
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
@@ -212,16 +172,14 @@ where {
         ExternRef { value: handle }
     }
 
-    pub fn invoke_and_return_bigint(&self, params: &[InvokeParam]) -> i64
-where {
+    pub fn invoke_and_return_bigint(&self, params: &[InvokeParam]) -> i64 {
         let param_bytes = param_to_bytes(params);
         let mut me = ManuallyDrop::new(param_bytes);
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
         unsafe { js_invoke_function_and_return_bigint(self.fn_handle, ptr, length) }
     }
 
-    pub fn invoke_and_return_string(&self, params: &[InvokeParam]) -> String
-where {
+    pub fn invoke_and_return_string(&self, params: &[InvokeParam]) -> String {
         let param_bytes = param_to_bytes(params);
         let mut me = ManuallyDrop::new(param_bytes);
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
@@ -230,8 +188,7 @@ where {
         crate::utils::allocations::extract_string_from_memory(allocation_id)
     }
 
-    pub fn invoke_and_return_array_buffer(&self, params: &[InvokeParam]) -> Vec<u8>
-where {
+    pub fn invoke_and_return_array_buffer(&self, params: &[InvokeParam]) -> Vec<u8> {
         let param_bytes = param_to_bytes(params);
         let mut me = ManuallyDrop::new(param_bytes);
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
@@ -246,15 +203,5 @@ where {
         let (ptr, length, _capacity) = (me.as_mut_ptr(), me.len(), me.capacity());
         let ret = unsafe { js_invoke_function_and_return_bool(self.fn_handle, ptr, length) };
         ret != 0.0
-    }
-}
-
-pub fn register_function(code: &str) -> JSFunction {
-    let start = code.as_ptr();
-    let len = code.len();
-    unsafe {
-        JSFunction {
-            fn_handle: js_register_function(start as usize as f64, len as f64),
-        }
     }
 }
