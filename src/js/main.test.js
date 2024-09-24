8a2d83b0a4d0cfab95083c7fc35c923a27124ca7
Andreas Tzionis <andy0n7@gmail.com>
2024-09-24 11:35:45 +0300

    minor fix

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -12,10 +12,9 @@ test('check read params', () => {
     ]
     for (const testCase of testCases) {
         _wasmModule.instance = { exports: { memory: { buffer: testCase.buffer } } }
-    
+        
         const result = readParams(0, testCase.buffer.length)
         assert.deepStrictEqual(result, testCase.result)
-
     }
 })
 
