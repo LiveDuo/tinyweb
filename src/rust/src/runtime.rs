047ec0a81a9cc9190d4b30af2adfca94e2d8118d
Andreas Tzionis <andy0n7@gmail.com>
2024-10-02 15:34:37 +0300

    minor fix

--- src/rust/src/runtime.rs
+++ src/rust/src/runtime.rs
@@ -9,18 +9,18 @@ use std::{
 
 use crate::bindings::window::set_timeout;
 
-fn clone_arc_raw<T: Send>(data: *const ()) -> RawWaker {
+fn clone_arc_raw<T>(data: *const ()) -> RawWaker {
     let _arc = unsafe { ManuallyDrop::new(Arc::<T>::from_raw(data as *const T)).clone() };
     RawWaker::new(data, waker_vtable::<T>())
 }
-fn wake_arc_raw<T: Send>(_data: *const ()) {
+fn wake_arc_raw(_data: *const ()) {
     set_timeout(|| { DEFAULT_RUNTIME.lock().unwrap().poll_tasks(); }, 0);
 }
 fn drop_arc_raw<T>(data: *const ()) {
     unsafe { drop(Arc::<T>::from_raw(data as *const T)) }
 }
-fn waker_vtable<W: Send>() -> &'static RawWakerVTable {
-    &RawWakerVTable::new(clone_arc_raw::<W>, wake_arc_raw::<W>, wake_arc_raw::<W>, drop_arc_raw::<W>)
+fn waker_vtable<T>() -> &'static RawWakerVTable {
+    &RawWakerVTable::new(clone_arc_raw::<T>, wake_arc_raw, wake_arc_raw, drop_arc_raw::<T>)
 }
 
 trait Pendable {
