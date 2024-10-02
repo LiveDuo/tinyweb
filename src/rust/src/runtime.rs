4d4179c99f9524479d18c249217ddbb69950db9a
Andreas Tzionis <andy0n7@gmail.com>
2024-10-02 15:16:35 +0300

    restore runtime

--- src/rust/src/runtime.rs
+++ src/rust/src/runtime.rs
@@ -9,12 +9,28 @@ use std::{
 
 use crate::bindings::window::set_timeout;
 
+fn clone_arc_raw<T: Send>(data: *const ()) -> RawWaker {
+    let _arc = unsafe { ManuallyDrop::new(Arc::<T>::from_raw(data as *const T)).clone() };
+    RawWaker::new(data, waker_vtable::<T>())
+}
+fn wake_arc_raw<T: Send>(_data: *const ()) {
+    set_timeout(|| { DEFAULT_RUNTIME.lock().unwrap().poll_tasks(); }, 0);
+}
+fn drop_arc_raw<T>(data: *const ()) {
+    unsafe { drop(Arc::<T>::from_raw(data as *const T)) }
+}
+fn waker_vtable<W: Send>() -> &'static RawWakerVTable {
+    &RawWakerVTable::new(clone_arc_raw::<W>, wake_arc_raw::<W>, wake_arc_raw::<W>, drop_arc_raw::<W>)
+}
+
 trait Pendable {
     fn is_pending(&self) -> bool;
 }
 
+type TasksList = VecDeque<Box<dyn Pendable + Send>>;
+
 pub struct Runtime {
-    tasks: VecDeque<Box<dyn Pendable + Send>>,
+    tasks: Option<TasksList>,
 }
 
 struct Task<T> {
@@ -25,22 +41,9 @@ impl<T> Pendable for Arc<Task<T>> {
     fn is_pending(&self) -> bool {
         let mut future = self.future.lock().unwrap();
 
-        fn clone_arc_raw<W>(data: *const ()) -> RawWaker {
-            RawWaker::new(data, waker_vtable::<W>())
-        }
-        fn wake_arc_raw(_data: *const ()) {
-            set_timeout(|| { DEFAULT_RUNTIME.lock().unwrap().poll_tasks(); }, 0);
-        }
-        fn drop_arc_raw<W>(data: *const ()) {
-            unsafe { drop(Arc::<W>::from_raw(data as *const W)) }
-        }
-        fn waker_vtable<W>() -> &'static RawWakerVTable {
-            &RawWakerVTable::new(clone_arc_raw::<W>, wake_arc_raw, wake_arc_raw, drop_arc_raw::<W>)
-        }
-
         let ptr = (&**self as *const Task<T>) as *const ();
-        let raw_waker = RawWaker::new(ptr, waker_vtable::<Task<T>>());
-        let waker = ManuallyDrop::new(unsafe { Waker::from_raw(raw_waker) });
+        let waker =
+            ManuallyDrop::new(unsafe { Waker::from_raw(RawWaker::new(ptr, waker_vtable::<Task<T>>())) });
         let context = &mut Context::from_waker(&*waker);
         matches!(future.as_mut().poll(context), Poll::Pending)
     }
@@ -50,35 +53,50 @@ impl Runtime {
 
     fn add_task<T: Send + 'static>(&mut self, future: Pin<Box<dyn Future<Output = T> + Send + 'static>>) {
         let task = Arc::new(Task { future: Mutex::new(future), });
-        self.tasks.push_back(Box::new(task));
+        if self.tasks.is_none() {
+            self.tasks = Some(TasksList::new());
+        }
+        let tasks: &mut TasksList = self.tasks.as_mut().expect("tasks not initialized");
+        tasks.push_back(Box::new(task));
     }
 
     fn poll_tasks(&mut self) {
-        if self.tasks.is_empty() {
-            return;
+        if self.tasks.is_none() {
+            self.tasks = Some(TasksList::new());
         }
 
-        for _ in 0..self.tasks.len() {
-            let task = self.tasks.pop_front().unwrap();
+        let tasks: &mut TasksList = self.tasks.as_mut().expect("tasks not initialized");
+        if tasks.is_empty() { return; }
+
+        for _ in 0..tasks.len() {
+            let task = tasks.pop_front().unwrap();
             if task.is_pending() {
-                self.tasks.push_back(task);
+                tasks.push_back(task);
             }
         }
     }
 }
 
-static DEFAULT_RUNTIME: Mutex<Runtime> = Mutex::new(Runtime { tasks: VecDeque::new() });
+static DEFAULT_RUNTIME: Mutex<Runtime> = Mutex::new(Runtime { tasks: None });
 
 pub fn run<T: Send + 'static>(future: impl Future<Output = T> + Send + 'static) {
     DEFAULT_RUNTIME.lock().map(|mut s| {
         s.add_task(Box::pin(future));
         s.poll_tasks();
-    }).unwrap()
+    }).unwrap();
 }
 
 pub fn coroutine<T: Send + 'static>(future: impl Future<Output = T> + Send + 'static) {
     let mut a = Some(Box::pin(future));
-    set_timeout(move || { if let Some(b) = a.take() { run(b); }}, 0);
+    set_timeout(
+        move || {
+            let b = a.take();
+            if let Some(b) = b {
+                run(b);
+            }
+        },
+        0,
+    );
 }
 
 
@@ -94,8 +112,7 @@ mod tests {
         let future = async move {
             has_run_clone.lock().map(|mut s| { *s = true; }).unwrap();
         };
-        run(Box::pin(future));
-
+        DEFAULT_RUNTIME.lock().unwrap().run(Box::pin(future));
         assert_eq!(*has_run.lock().unwrap(), true);
     }
 
