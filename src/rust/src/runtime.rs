f624f698abbe0d70ca35dd8e176576bcc5e2839f
Andreas Tzionis <andy0n7@gmail.com>
2024-10-02 15:28:28 +0300

    minor fix

--- src/rust/src/runtime.rs
+++ src/rust/src/runtime.rs
@@ -40,8 +40,8 @@ impl<T> Pendable for Arc<Task<T>> {
         let mut future = self.future.lock().unwrap();
 
         let ptr = (&**self as *const Task<T>) as *const ();
-        let waker =
-            ManuallyDrop::new(unsafe { Waker::from_raw(RawWaker::new(ptr, waker_vtable::<Task<T>>())) });
+        let raw_waker = RawWaker::new(ptr, waker_vtable::<Task<T>>());
+        let waker = ManuallyDrop::new(unsafe { Waker::from_raw(raw_waker) });
         let context = &mut Context::from_waker(&*waker);
         matches!(future.as_mut().poll(context), Poll::Pending)
     }
@@ -49,15 +49,7 @@ impl<T> Pendable for Arc<Task<T>> {
 
 impl Runtime {
 
-    fn add_task<T: Send + 'static>(&mut self, future: Pin<Box<dyn Future<Output = T> + Send + 'static>>) {
-        let task = Arc::new(Task { future: Mutex::new(future), });
-        self.tasks.push_back(Box::new(task));
-    }
-
     fn poll_tasks(&mut self) {
-
-        if self.tasks.is_empty() { return; }
-
         for _ in 0..self.tasks.len() {
             let task = self.tasks.pop_front().unwrap();
             if task.is_pending() {
@@ -71,7 +63,8 @@ static DEFAULT_RUNTIME: Mutex<Runtime> = Mutex::new(Runtime { tasks: VecDeque::n
 
 pub fn run<T: Send + 'static>(future: impl Future<Output = T> + Send + 'static) {
     DEFAULT_RUNTIME.lock().map(|mut s| {
-        s.add_task(Box::pin(future));
+        let task = Task { future: Mutex::new(Box::pin(future)) };
+        s.tasks.push_back(Box::new(Arc::new(task)));
         s.poll_tasks();
     }).unwrap();
 }
