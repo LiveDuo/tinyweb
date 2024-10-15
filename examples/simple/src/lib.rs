ae2a4b12ec4d9072fc387c2a06558cbcc59c03e4
Andreas Tzionis <andy0n7@gmail.com>
2024-10-15 11:23:06 +0300

    reproduce

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,5 +1,6 @@
 
-use tinyweb::element::El;
+use tinyweb::signals::Signal;
+
 use tinyweb::bindings::{console, dom};
 
 #[no_mangle]
@@ -7,10 +8,18 @@ pub fn main() {
 
     std::panic::set_hook(Box::new(|e| console::console_log(&e.to_string())));
 
-    let body = dom::query_selector("body");
-    let page = El::new("div")
-        .child(El::new("button").text("button 1").on_click(move |_| { console::console_log("1"); }))
-        .child(El::new("button").text("button 2").on_click(move |_| { console::console_log("2"); }));
-    page.mount(&body);
+    let signal = Signal::new("title".to_owned());
+    signal.on(|_v| {
+        let button = dom::create_element("button");
+        dom::element_add_click_listener(&button, |_s| {}); // thows error
+    });
 
+    let signal_clone = signal.clone();
+    let button = dom::create_element("button");
+    let button_text = dom::create_text_node("Click");
+    dom::append_child(&button, &button_text);
+    dom::element_add_click_listener(&button, move |_s| { signal_clone.set("2".to_owned()); });
+
+    let body = dom::query_selector("body");
+    dom::append_child(&body, &button);
 }
