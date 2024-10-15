3d7d1bca01d2134f6746a12fb537c6bceb3851e9
Andreas Tzionis <andy0n7@gmail.com>
2024-10-15 21:31:42 +0300

    attempt fix

--- src/rust/src/bindings/dom.rs
+++ src/rust/src/bindings/dom.rs
@@ -172,35 +172,22 @@ pub struct EventHandler<T> {
 
 impl<T> EventHandler<T> {
     pub fn add_listener(&self, id: Rc<ExternRef>, handler: Box<dyn FnMut(T) + 'static>) {
-
-        super::console::console_log("add_listener");
-
-        self.listeners.lock().map(|mut s| {
-            s.insert(id, handler);
-        }).unwrap();
-
-        super::console::console_log("add_listener after");
+        self.listeners.lock().map(|mut s| { s.insert(id, handler); }).unwrap();
     }
 
     pub fn remove_listener(&self, id: &Rc<ExternRef>) {
-
-        super::console::console_log("remove_listener");
-
         let mut handlers = self.listeners.lock().unwrap();
         handlers.remove(id);
     }
 
     pub fn call(&self, id: i64, event: T) {
 
-        super::console::console_log("call");
+        let handler = self.listeners.lock().map(|mut s| {
+            let (_, handler) = s.iter_mut().find(|(s, _)| s.value == id as u32).unwrap();
+            handler as *mut Box<dyn FnMut(T) + 'static>
+        }).unwrap();
 
-        let mut handlers = self.listeners.lock().unwrap();
-        for (key, handler) in handlers.iter_mut() {
-            if key.value == id as u32 {
-                handler(event);
-                return;
-            }
-        }
+        unsafe { (*handler)(event) }
     }
 }
 
@@ -216,16 +203,12 @@ thread_local! {
 #[no_mangle]
 pub extern "C" fn web_handle_mouse_event_handler(id: i64, x: f64, y: f64) {
 
-    super::console::console_log("web_handle_mouse_event_handler");
-
     MOUSE_EVENT_HANDLER.with(|s| {
         s.call(id, MouseEvent { offset_x: x, offset_y: y });
     })
 }
 
 pub fn element_add_click_listener(element: &ExternRef, handler: impl FnMut(MouseEvent) + 'static) -> Rc<ExternRef> {
-
-    super::console::console_log("element_add_click_listener");
 
     let function_ref = JsFunction::register(r#"
         function(element ){
