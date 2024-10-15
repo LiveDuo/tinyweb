2bd45b71c2be136dc2baf2c77381aaf2b244da9f
Andreas Tzionis <andy0n7@gmail.com>
2024-10-15 15:15:02 +0300

    simplify

--- examples/simple/src/lib.rs
+++ examples/simple/src/lib.rs
@@ -1,6 +1,4 @@
 
-use tinyweb::signals::Signal;
-
 use tinyweb::bindings::{console, dom};
 
 #[no_mangle]
@@ -8,17 +6,13 @@ pub fn main() {
 
     std::panic::set_hook(Box::new(|e| console::console_log(&e.to_string())));
 
-    let signal = Signal::new("title".to_owned());
-    signal.on(|_v| {
-        let button = dom::create_element("button");
-        dom::element_add_click_listener(&button, |_s| {}); // thows error
-    });
-
-    let signal_clone = signal.clone();
     let button = dom::create_element("button");
     let button_text = dom::create_text_node("Click");
     dom::append_child(&button, &button_text);
-    dom::element_add_click_listener(&button, move |_s| { signal_clone.set("2".to_owned()); });
+    dom::element_add_click_listener(&button, move |_s| {
+        let button = dom::create_element("button");
+        dom::element_add_click_listener(&button, |_s| {}); // thows error
+    });
 
     let body = dom::query_selector("body");
     dom::append_child(&body, &button);
