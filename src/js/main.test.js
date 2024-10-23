f5df0d1c2ad9b1bff1e169b8b935b34627be0564
Andreas Tzionis <andy0n7@gmail.com>
2024-10-23 16:47:12 +0300

    minor fix

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -6,21 +6,19 @@ const { readParamsFromMemory, wasmModule } = require('./main')
 // node src/js/main.test.js
 test('check read params', () => {
 
-    const float64View = new DataView(new ArrayBuffer(1 + 8))
-    float64View.setInt8(0, 2)
-    float64View.setFloat64(1, 42.42, true)
+    const float64View = new DataView(new ArrayBuffer(8))
+    float64View.setFloat64(0, 42.42, true)
     const float64Array = new Uint8Array(float64View.buffer)
 
-    const bigInt64View = new DataView(new ArrayBuffer(1 + 8))
-    bigInt64View.setInt8(0, 3)
-    bigInt64View.setBigInt64(1, 42n, true)
+    const bigInt64View = new DataView(new ArrayBuffer(8))
+    bigInt64View.setBigInt64(0, 42n, true)
     const bigInt64Array = new Uint8Array(bigInt64View.buffer)
 
     const testCases = [
         {memory: [0], expected: [undefined]},
         {memory: [1], expected: [null]},
-        {memory: float64Array, expected: [42.42]},
-        {memory: bigInt64Array, expected: [42n]}
+        {memory: [2, ...float64Array], expected: [42.42]},
+        {memory: [3, ...bigInt64Array], expected: [42n]}
     ]
     for (const testCase of testCases) {
         wasmModule.instance = { exports: { memory: { buffer: testCase.memory } } }
