c113dac39494834c3ae640e13ba0a5699019b254
Andreas Tzionis <andy0n7@gmail.com>
2024-10-23 16:45:05 +0300

    added test case

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -11,10 +11,16 @@ test('check read params', () => {
     float64View.setFloat64(1, 42.42, true)
     const float64Array = new Uint8Array(float64View.buffer)
 
+    const bigInt64View = new DataView(new ArrayBuffer(1 + 8))
+    bigInt64View.setInt8(0, 3)
+    bigInt64View.setBigInt64(1, 42n, true)
+    const bigInt64Array = new Uint8Array(bigInt64View.buffer)
+
     const testCases = [
         {memory: [0], expected: [undefined]},
         {memory: [1], expected: [null]},
-        {memory: float64Array, expected: [42.42]}
+        {memory: float64Array, expected: [42.42]},
+        {memory: bigInt64Array, expected: [42n]}
     ]
     for (const testCase of testCases) {
         wasmModule.instance = { exports: { memory: { buffer: testCase.memory } } }
