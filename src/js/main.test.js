24a2579f5ff15faf6165540999c812b3bc7bcdd2
Andreas Tzionis <andy0n7@gmail.com>
2024-10-23 20:15:41 +0300

    worked on js test

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -14,13 +14,26 @@ test('check read params', () => {
     bigInt64View.setBigInt64(0, 42n, true)
     const bigInt64Array = new Uint8Array(bigInt64View.buffer)
 
+    const int32View = new DataView(new ArrayBuffer(4))
+    int32View.setInt32(0, 42, true)
+    const int32Array = new Uint8Array(int32View.buffer)
+
+    const uint32View = new DataView(new ArrayBuffer(4))
+    uint32View.setInt32(0, 42, true)
+    const uint32Array = new Uint8Array(uint32View.buffer)
+
     const testCases = [
         {memory: [0], expected: [undefined]},
         {memory: [1], expected: [null]},
         {memory: [2, ...float64Array], expected: [42.42]},
         {memory: [3, ...bigInt64Array], expected: [42n]},
+        {memory: [4, ...int32Array, ...int32Array], expected: ['']},
+        {memory: [5, ...uint32Array], expected: [undefined]},
+        {memory: [6, ...int32Array, ...int32Array], expected: [new Float32Array(0)]},
         {memory: [7], expected: [true]},
-        {memory: [8], expected: [false]}
+        {memory: [8], expected: [false]},
+        {memory: [9, ...int32Array, ...int32Array], expected: [new Float64Array(0)]},
+        {memory: [10, ...int32Array, ...int32Array], expected: [new Uint32Array(0)]},
     ]
     for (const testCase of testCases) {
         wasmModule.instance = { exports: { memory: { buffer: testCase.memory } } }
