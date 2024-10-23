688e3b3b55b70751f82ab4ab2078ac022ebc8117
Andreas Tzionis <andy0n7@gmail.com>
2024-10-23 16:42:06 +0300

    added test case

--- src/js/main.test.js
+++ src/js/main.test.js
@@ -6,9 +6,15 @@ const { readParamsFromMemory, wasmModule } = require('./main')
 // node src/js/main.test.js
 test('check read params', () => {
 
+    const float64View = new DataView(new ArrayBuffer(1 + 8))
+    float64View.setInt8(0, 2)
+    float64View.setFloat64(1, 42.42, true)
+    const float64Array = new Uint8Array(float64View.buffer)
+
     const testCases = [
         {buffer: [0], result: [undefined]},
-        {buffer: [1], result: [null]}
+        {buffer: [1], result: [null]},
+        {buffer: float64Array, result: [42.42]}
     ]
     for (const testCase of testCases) {
         wasmModule.instance = { exports: { memory: { buffer: testCase.buffer } } }
