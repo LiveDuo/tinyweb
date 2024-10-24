24a701b56c8018c31d820cdf05861f10eeae91bb
Andreas Tzionis <andy0n7@gmail.com>
2024-10-24 23:33:40 +0300

    minor fix

--- src/rust/src/bindings/http.rs
+++ src/rust/src/bindings/http.rs
@@ -97,20 +97,9 @@ impl XMLHttpRequest {
     }
 }
 
-pub enum HTTPMethod {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    OPTIONS,
-    PATCH,
-}
+pub enum HTTPMethod { GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH, }
 
-pub enum FetchResponse {
-    Text(u32, String),
-    ArrayBuffer(u32, Vec<u8>),
-}
+pub enum FetchResponse { Text(u32, String), ArrayBuffer(u32, Vec<u8>), }
 
 pub struct FetchOptions<'a> {
     pub url: &'a str,
@@ -138,14 +127,7 @@ impl Default for FetchOptions<'_> {
 }
 
 pub fn fetch(options: FetchOptions) -> impl Future<Output = FetchResponse> {
-    let url = options.url;
-    let body = options.body;
-    let headers = options.headers;
-    let response_type = options.response_type;
-    let action = options.action;
-    let request = Rc::new(XMLHttpRequest::new());
-    let r2 = request.clone();
-    let method_str = match action {
+    let method_str = match options.action {
         HTTPMethod::GET => "GET",
         HTTPMethod::POST => "POST",
         HTTPMethod::PUT => "PUT",
@@ -154,18 +136,19 @@ pub fn fetch(options: FetchOptions) -> impl Future<Output = FetchResponse> {
         HTTPMethod::OPTIONS => "OPTIONS",
         HTTPMethod::PATCH => "PATCH",
     };
-    request.open(method_str, &url);
-    if let Some(body) = body {
+    let request = Rc::new(XMLHttpRequest::new());
+    request.open(method_str, &options.url);
+    if let Some(body) = options.body {
         request.send_with_body(&body);
     } else {
         request.send();
     }
-    if let Some(headers) = headers {
+    if let Some(headers) = options.headers {
         for (key, value) in headers {
             request.set_request_header(&key, &value);
         }
     }
-    match response_type {
+    match options.response_type {
         FetchResponseType::Text => {
             request.set_response_type("text");
         }
@@ -174,8 +157,9 @@ pub fn fetch(options: FetchOptions) -> impl Future<Output = FetchResponse> {
         }
     }
 
+    let r2 = request.clone();
     let (future, state_id) = EventHandlerFuture::<FetchResponse>::create_future_with_state_id();
-    request.set_on_load(move || match response_type {
+    request.set_on_load(move || match options.response_type {
         FetchResponseType::Text => {
             let status = r2.response_status();
             let text = r2.response_text();
